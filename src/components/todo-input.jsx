19487b66173df31c99cbd5ac61e623fd302673a6
Aryab13 <223210059@student.unaki.ac.id>
2023-10-30 11:49:05 +0700

    components add

--- src/components/todo-input.jsx
+++ src/components/todo-input.jsx
@@ -1,12 +1,50 @@
+import { useContext } from "react";
+import { TodoContext } from "../context/todo-provider";
+
 function TodoInput() {
+    const {
+        todos,
+        setTodos,
+        todoInput,
+        setTodoInput,
+        isEdit,
+        todoEdit,
+        setIsEdit,
+      } = useContext(TodoContext);
+
+      const handleClick = (e) => {
+        e.preventDefault();
+    
+        if (isEdit) {
+          let cloneTodo = [...todos];
+          let index = cloneTodo.findIndex((item) => item.id == todoEdit.id);
+          cloneTodo[index].value = todoInput;
+    
+          setTodos(cloneTodo);
+          setIsEdit(false);
+        } else {
+          let newTodo = {
+            id: new Date(),
+            value: todoInput,
+            status: false,
+          };
+    
+          setTodos([...todos, newTodo]);
+        }
+    
+        setTodoInput("");
+      };
+
     return (
       <>
         <form>
             <input
             type="text"
+            value={todoInput}
             placeholder="What to do"
+            onChange={(e) => setTodoInput(e.target.value)}
             />
-            <button>Add</button>
+            <button onClick={handleClick}>{isEdit ? "Edit" : "Add"}</button>
         </form>
       </>
     )
--- src/components/todo-list.jsx
+++ src/components/todo-list.jsx
@@ -1,14 +1,43 @@
+import { useContext } from "react";
+import { TodoContext } from "../context/todo-provider";
+
 function TodoList (){
+    const { todos, setTodos, setTodoInput, setIsEdit, setTodoEdit } =
+    useContext(TodoContext);
+
+    const handleStatus = (index) => {
+        let cloneTodos = [...todos];
+        cloneTodos[index].status = !cloneTodos[index].status;
+    
+        setTodos([...cloneTodos]);
+      };
+    
+      const handleEdit = (todo) => {
+        setTodoEdit(todo);
+        setTodoInput(todo.value);
+        setIsEdit(true);
+      };
+
+      const handleDelete = (todo) => {
+        let cloneTodos = [...todos];
+        cloneTodos.filter((item) => item.id != todos.id);
+
+      }
+
     return (
         <>
         <div>
-            <div>
-                <span></span>
+            {todos.map((todo, index) => (
+            <div key={todo.id}>
+                <span onClick={()=> handleStatus(index)}>
+                    {todo.value}
+                </span>
                 <div>
-                    <button></button>
+                    <button onClick={()=>handleEdit(todo)}>✏️</button>
                     <button></button>
                 </div>
             </div>
+            ))}
         </div>
         </>
     )
