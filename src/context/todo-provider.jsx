a2c71a1914fd8458d7286c5467824d38f1ee665e
Aryab13 <223210059@student.unaki.ac.id>
2023-10-30 11:23:50 +0700

    create context

---
+++ src/context/todo-provider.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+export const TodoContext = createContext();
+
+function TodoProvider({ children }) {
+  const [todos, setTodos] = useState([
+    { id: 1, value: "Buy Milk", status: false },
+    { id: 2, value: "Buy Egg", status: false },
+    { id: 3, value: "Buy T-Shirt", status: false },
+  ]);
+  const [todoInput, setTodoInput] = useState("");
+  const [todoEdit, setTodoEdit] = useState({});
+
+  const [isEdit, setIsEdit] = useState("");
+
+  return (
+    <TodoContext.Provider value={{ todos, setTodos, todoInput, setTodoInput, isEdit, setIsEdit, todoEdit, setTodoEdit }}>
+      {children}
+    </TodoContext.Provider>
+  );
+}
+
+export default TodoProvider;
